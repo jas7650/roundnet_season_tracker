183da5387ba764d3cec7a19b03f730a676bb058d
Justin Shaytar <jas7650@rit.edu>
2023-03-24 23:41:34 -0400

    Added helper functions that will be used in the future

--- Player.py
+++ Player.py
@@ -1,10 +1,48 @@
 
 
 
-class Player:
+class Player(object):
 
     def __init__(self, name):
         self.name = name
+        self.tournaments = []
+        self.teams = []
+        self.results = []
 
+    def addTournament(self, tournament):
+        if tournament not in self.tournaments:
+            self.tournaments.append(tournament)
+
+    def addTeam(self, team):
+        if team not in self.teams:
+            self.teams.append(team)
+
+    def addResult(self, amount):
+        index = 0
+        while amount < self.results[index]:
+            index += 1
+        self.results.insert(index, amount)
+
+    def getName(self):
+        return self.name
+
+    def getTournaments(self):
+        return self.tournaments
+    
+    def getTeams(self):
+        return self.teams
+    
+    def getResults(self):
+        return self.results
+
+    def equals(self, player):
+        if self.name == player.name:
+            return True
+        else:
+            return False
+        
     def printPlayer(self):
         print(f'Name: {self.name}')
+        print(f'Teams: {self.teams}')
+        print(f'Tournaments: {self.tournaments}')
+        print(f'Results: {self.results}')
