00ce8e9df1cefa311afd2b6f9d37f9fd0bbb87dd
Justin Shaytar <justin.shaytar@caci.com>
2023-04-07 15:55:33 -0400

    Added point support for 2023

--- utils/point_utils.py
+++ utils/point_utils.py
@@ -12,6 +12,14 @@ championship_2022 = [1500, 900, 570, 405, 330, 300, 270, 255, 240, 225, 210, 195
 contender_2022 = [150, 90, 57, 40.5, 33, 30, 27, 25.5, 24, 22.5, 21, 19.5, 18, 17.1, 16.5, 15.9, 15.3, 14.7, 14.1, 13.5, 12.9, 12.3, 11.7, 11.1, 10.65, 10.2, 9.75, 9.3, 8.85, 8.4, 7.95, 7.5, 7.05, 6.6, 6.3, 6, 5.7, 5.4, 5.1, 4.8, 4.5, 4.2, 3.9, 3.6, 3.3, 3.15, 3, 2.85, 2.7, 2.55, 2.4, 2.25, 2.1, 1.95, 1.8, 1.74, 1.68, 1.62, 1.56, 1.5, 1.44, 1.38, 1.32, 1.26, 1.2, 1.14, 1.08, 1.02, 0.96, 0.9, 0.87, 0.84, 0.81, 0.78, 0.75, 0.72, 0.69, 0.66, 0.63, 0.6, 0.57, 0.54, 0.51, 0.48, 0.45]
 points_2022 = [challenger_2022, major_2022, championship_2022, contender_2022]
 
+challenger_2023_1_7 = [150, 90, 57, 40.5, 33, 30, 27]
+challenger_2023_8_16 = [300, 180, 114, 81, 66, 60, 54, 51, 48, 45, 42, 39, 36, 34.2, 33, 31.8]
+challenger_2023_17_32 = [500, 300, 190, 135, 110, 100, 90, 85, 80, 75, 70, 65, 60, 57, 55, 53, 51, 49, 47, 45, 43, 41, 39, 37, 35.5, 34, 32.5, 31, 29.5, 28, 26.5, 25]
+challenger_2023_33 = [600, 360, 228, 162, 132, 120, 108, 102, 96, 90, 84, 78, 72, 68.4, 66, 63.6, 61.2, 58.8, 56.4, 54, 51.6, 49.2, 46.8, 44.4, 42.6, 40.8, 39, 37.2, 35.4, 33.6, 31.8, 30]
+major_2023 = [1100, 660, 418, 297, 242, 220, 198, 187, 176, 165, 154, 143, 132, 125.4, 121, 116.6, 112.2, 107.8, 103.4, 99, 94.6, 90.2, 85.8, 81.4, 78.1, 74.8, 71.5, 68.2, 64.9, 61.6, 58.3, 55]
+championship_2023 = [1600, 960, 608, 432, 352, 320, 288, 272, 256, 240, 224, 208, 192, 182.4, 176, 169.6, 163.2, 156.8, 150.4, 144, 137.6, 131.2, 124.8, 118.4, 113.6, 108.8, 104, 99.2, 94.4, 89.6, 84.8, 80]
+points_2023 = [challenger_2023_1_7, challenger_2023_8_16, challenger_2023_17_32, challenger_2023_33, major_2023, championship_2023]
+
 
 def getTournamentType(type : str):
     if type == "challenger":
@@ -23,13 +31,27 @@ def getTournamentType(type : str):
     return TournamentType.CONTENDER.value
 
 
-def getPoints(tounamentType : int, hasPremier: bool, year : int, ranks : list):
+def getPoints(tounamentType : int, year : int, ranks : list, numTeams : int):
     if year == 2022:
         index = tounamentType
         points = []
         for point in points_2022[index]:
             points.append(point/2.0)
         return getActualPoints(ranks, points)
+    if year == 2023:
+        if tounamentType == TournamentType.CHALLENGER.value:
+            if numTeams < 8:
+                return getActualPoints(ranks, points_2023[0])
+            if numTeams >= 8 and numTeams < 17:
+                return getActualPoints(ranks, points_2023[1])
+            if numTeams > 16 and numTeams < 33:
+                return getActualPoints(ranks, points_2023[2])
+            if numTeams > 32:
+                return getActualPoints(ranks, points_2023[3])
+        if tounamentType == TournamentType.MAJOR.value:
+            return getActualPoints(ranks, points_2023[4])
+        if tounamentType == TournamentType.CHAMPIONSHIP.value:
+            return getActualPoints(ranks, points_2023[5])
     
 
 def getActualPoints(ranks, points):
